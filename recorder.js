007f54124f85228e06b6eba9e33d950d98b75e7f
Leo Tunkle <tunkleo@umich.edu>
2021-03-11 16:28:52 -0500

    got play working for multiple recordings

--- recorder.js
+++ recorder.js
@@ -10,15 +10,16 @@ navigator.mediaDevices.getUserMedia({ audio: true })
 .then(stream => {
   const mediaRecorder = new MediaRecorder(stream);
 
-  // links the start button in the html file to start recording
-  document.getElementById("start").addEventListener("click", function(){
-    var node = document.getElementById('ziqi');
-    node.innerHTML = '<p>HELLO ZIQI!!!</p>';
-    mediaRecorder.start();
-  });
-
   // creates an array to store chunks of sequential audio data
   const audioChunks = [];
+
+  // links the start button in the html file to start recording
+  document.getElementById("start").addEventListener("click", function(){
+    // var node = document.getElementById('ziqi');
+    // node.innerHTML = '<p>HELLO ZIQI!!!</p>';
+    audioChunks.length = 0;
+    mediaRecorder.start();
+  });
 
   // adds data to the array as it comes... but we don't really understand this
   mediaRecorder.addEventListener("dataavailable", event => {
@@ -27,9 +28,15 @@ navigator.mediaDevices.getUserMedia({ audio: true })
 
   // links the stop button in the html file to stop recording
   document.getElementById("stop").addEventListener("click", function(){
-    var node = document.getElementById('ziqi');
-    node.innerHTML = '<p>BYE ZIQI!!!</p>';
+    // var node = document.getElementById('ziqi');
+    // node.innerHTML = '<p>BYE ZIQI!!!</p>';
     mediaRecorder.stop();
+  });
+
+  // links the play button in the html file to play selected clips
+  document.getElementById("play").addEventListener("click", function(){
+    // helper function to only play selected audio
+    playSomething();
   });
 
   // processes recorded data after recording is stopped
@@ -39,27 +46,44 @@ navigator.mediaDevices.getUserMedia({ audio: true })
     // const audioUrl = URL.createObjectURL(audioBlob);
     // const audio = new Audio(audioUrl);
 
-    audioChunksArray.push(audioChunks);
+    // Add the current recording to the overall recording list
+    // Note the shallow copy! [...array]
+    audioChunksArray.push([...audioChunks]);
 
-    // converts the (array of audio data) audiochunks -> blob -> url -> arrayBuffer -> audioBuffer
-    process(audioChunks);
-
-    // checkboxManager();
+    // update the checkboxes in the html document
+    checkboxManager();
 
   });
 });
 
+// update the checkboxes in the html document
+function checkboxManager(){
+  var text = "";
+  for (i = 0; i < audioChunksArray.length; ++i){
+    text += "<input type=\"checkbox\" id=\"box" + i + "\" name=\"z\" value=\"" + i + "\"><br>";
+  }
+  var node = document.getElementById('recordlist');
+  node.innerHTML = text;
+}
 
-// function checkboxManager(){
-//   audioChunksArray.length
-//   for
-// }
+// helper function to only play selected audio
+function playSomething(){
+  for (i = 0; i < audioChunksArray.length; ++i){
+    var idname = "box" + i;
 
-
+    // only process (play) checked items
+    if (document.getElementById(idname).checked){
+      // converts the (array of audio data) audiochunks -> blob -> url -> arrayBuffer -> audioBuffer
+      process(audioChunksArray[i]);
+    }
+  }
+}
 
 // converts the (array of audio data) audiochunks -> blob -> url -> arrayBuffer -> audioBuffer 
 function process(data) {
   const blob = new Blob(data);
+
+  console.log(blob)
   
   convertToArrayBuffer(blob) // see function below
       .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer)) // convert from arraybuffer to audiobuffer
@@ -85,8 +109,8 @@ function play(audioBuffer) {
   // sourceNode.detune.value = -300;
   
   sourceNode.connect(audioContext.destination);
-  document.getElementById("play").addEventListener("click", function(){sourceNode.start()});
-  // sourceNode.start();
+  // document.getElementById("play").addEventListener("click", function(){sourceNode.start()});
+  sourceNode.start();
 }
 
 
