618d5d6b48a0c8ad92c7962f7941dc7b244c8c32
ziqiw <ziqiw@umich.edu>
2021-03-20 21:22:31 -0400

    add download

--- recorder.js
+++ recorder.js
@@ -75,6 +75,10 @@ navigator.mediaDevices.getUserMedia({ audio: {
         deleteSomething();
     });
     
+    document.getElementById("download").addEventListener("click", function() {
+        // helper function to only play selected audio
+        downloadSomething();
+    });
     
     
     // processes recorded data after recording is stopped
@@ -164,6 +168,32 @@ function deleteSomething() {
     checkboxManager();
 }
 
+function downloadSomething() {
+    // prevent the buffer array length from changing while deleting
+    for (i = 0; i < audioBufferArray.length; ++i){
+        var idname = "box" + i;
+        // only process (play) checked items
+        // the count is an offest when you delete multiple at once
+        if (document.getElementById(idname).checked){
+            myDownload(audioBufferArray[i]);
+        }
+    }
+}
+
+function myDownload(buffer, filename) {
+    const type = "audio/wav";
+    const recorded = interleave(buffer);
+    const dataview = writeHeaders(recorded);
+    const audioBlob = new Blob([dataview], { type: type });
+    const name = filename || "PochetChoir";
+    const a = document.createElement("a");
+    a.style = "display: none";
+    a.href = renderURL(audioBlob);
+    a.download = `${name}.${audioBlob.type.split("/")[1]}`;
+    a.click();
+    return a;
+}
+
 // converts the (array of audio data) audiochunks -> blob -> url -> arrayBuffer -> audioBuffer 
 // and pushes into audioBufferArray
 function addAudioBuffer(data) {
@@ -250,3 +280,57 @@ function maxNumberOfChannels(buffers) {
                           );
 }
 
+function interleave(input) {
+    let buffer = input.getChannelData(0),
+      length = buffer.length * 2,
+      result = new Float32Array(length),
+      index = 0,
+      inputIndex = 0;
+
+    while (index < length) {
+      result[index++] = buffer[inputIndex];
+      result[index++] = buffer[inputIndex];
+      inputIndex++;
+    }
+    return result;
+  }
+
+function writeHeaders(buffer) {
+    let arrayBuffer = new ArrayBuffer(44 + buffer.length * 2),
+      view = new DataView(arrayBuffer);
+
+    writeString(view, 0, "RIFF");
+    view.setUint32(4, 32 + buffer.length * 2, true);
+    writeString(view, 8, "WAVE");
+    writeString(view, 12, "fmt ");
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+    view.setUint16(22, 2, true);
+    view.setUint32(24, this._sampleRate, true);
+    view.setUint32(28, this._sampleRate * 4, true);
+    view.setUint16(32, 4, true);
+    view.setUint16(34, 16, true);
+    writeString(view, 36, "data");
+    view.setUint32(40, buffer.length * 2, true);
+
+    return floatTo16BitPCM(view, buffer, 44);
+  }
+
+function renderURL(blob) {
+    return (window.URL || window.webkitURL).createObjectURL(blob);
+  }
+
+function writeString(dataview, offset, header) {
+    let output;
+    for (var i = 0; i < header.length; i++) {
+      dataview.setUint8(offset + i, header.charCodeAt(i));
+    }
+  }
+
+function floatTo16BitPCM(dataview, buffer, offset) {
+    for (var i = 0; i < buffer.length; i++, offset += 2) {
+      let tmp = Math.max(-1, Math.min(1, buffer[i]));
+      dataview.setInt16(offset, tmp < 0 ? tmp * 0x8000 : tmp * 0x7fff, true);
+    }
+    return dataview;
+  }
