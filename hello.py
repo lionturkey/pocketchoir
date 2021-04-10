7cd9cd45aa7e9b4b9365aac2a19d4317954446c9
Ubuntu <ubuntu@ip-172-31-16-176.us-east-2.compute.internal>
2021-04-10 15:44:53 +0000

    used symlinks to keep things working on git.io

--- hello.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask_cors import CORS
-
-
-if __name__ == "__main__":
-   app.run(debug=True)
-   # app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-   # cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:port"}})
